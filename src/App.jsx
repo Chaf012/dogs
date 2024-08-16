0359bc98c50974323613ae0fbbb24a3fe33c16d3
chafcodes <chafcodes@gmail.com>
2024-08-16 12:12:51 -0300

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
 
 <React></React>;
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
 
 const App = () => {
   return <div>App React</div>;
