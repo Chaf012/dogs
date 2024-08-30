278555d7ad012eb433b6d20a457ae4637ca90773
chafcodes <chafcodes@gmail.com>
2024-08-30 18:33:14 -0300

    ajustes name pages

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           <Header />
           <main className="AppBody">
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/dogs" element={<Home />} />
               <Route path="login/*" element={<Login />} />
               <Route
                 path="conta/*"
